3e62fbf7a5a44d3d5c8de4c68e2cb00e197aaa3b
유재욱 <jwyoo@jwyoo.local>
2022-03-02 23:13:42 +0900

    수정

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,16 +7,15 @@ const loginBt = document.querySelector("#loginBt");
 loginBt.addEventListener("click", login);
 
 function login() {
-    console.log("loginBt");
     const req = {
         id: id.value,
         pw: pw.value,
     };
     fetch("/login", {
         method: "POST",
-        header: {
+        headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(req)
+        body: JSON.stringify(req),
     });
 };
