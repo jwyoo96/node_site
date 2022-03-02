fc497809dcf3c0c0d6a0dfd1bb39d22a79b40950
유재욱 <jwyoo@jwyoo.local>
2022-03-02 22:57:18 +0900

    로그인을 요청하는 fetch생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -17,6 +17,6 @@ function login() {
         header: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify(req);
+        body: JSON.stringify(req)
     });
 };
